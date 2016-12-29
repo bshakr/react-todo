748cff99fb7ac8eabcd97f1fb28b292129277e8b
Bassem Shaker <bshakr@gmail.com>
2016-12-29 16:34:20 +0200

    Use enzyme to test shallow dom

--- spec/todo-list.spec.js
+++ spec/todo-list.spec.js
@@ -3,13 +3,13 @@ import renderer from 'react-test-renderer';
 import TodoList from '../src/todo-list';
 import TodoItem from '../src/todo-item';
 
-describe('empty', () => {
-  it('works', () => {
-    const todos = [{text: 'Buy milk', isComplete: false }];
-    function complete (){};
+describe('TodoList', () => {
+  const todos = [{text: 'Buy milk', isComplete: false }];
+  function complete (){};
+  
+  it('renders component using jest', () => {
     const todoList = renderer.create(<TodoList todos={todos} complete={complete} />);
     let tree = todoList.toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
-
